1365421f0bbb4203b9757c9e6a91118fdfde4bf5
adereme2 <aderme@mail.ru>
2025-01-28 13:32:30 +0400

    че то как то

--- модуль 2/лаб2/2.js
+++ модуль 2/лаб2/2.js
@@ -3,24 +3,22 @@
 // возведения числа в квадрат, а для нечётных – стрелочное выражение
 // возведения в куб.
 
-function powerFunction() {
-
-    let userInput = prompt('Введите целое число:');
-    let n = parseInt(userInput);
-
+function powerFunction(n) {
     if (n % 2 === 0) {
         console.log('Число четное, функция возводит в квадрат');
-        return (x) => x ** 2; 
-    } else {
+        return (x) => x ** 2;
+    }
+    else {
         console.log('Число нечетное, функция возводит в куб');
-        return (x) => x ** 3; 
+        return (x) => x ** 3;
     }
 }
 
 
-const powerFunc = powerFunction();
+let n = parseInt(prompt('Введите число'));
+
+const powerFunc = powerFunction(n);
 
 if (powerFunc) {
-    const testNumber = n; 
-    console.log(`Результат: ${powerFunc(testNumber)}`);
+    console.log(`Результат: ${powerFunc(n)}`);
 }
